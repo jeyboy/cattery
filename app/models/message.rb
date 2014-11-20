56ad78ea375d35288e8e31f7c31d9af4121febbd
Jenua Boiko <jeyboy1985@gmail.com>
2014-11-21 00:59:59 +0300

    fixes and improves

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ActiveRecord::Base
   validates :email, :name, :message_text, presence: true
+  validates :email, uniqueness: {scope: [:name, :message_text], message: 'You are wrote identical message already'}
 end
