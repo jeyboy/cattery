eb52a847a907566bc23743386d121e4d6a9d79ad
Jenua Boiko <jeyboy1985@gmail.com>
2014-11-27 01:12:29 +0300

    form inprovements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def init_form_fields_mandatory(f)
+    validator = f.object.class.validators.detect {|a| a.kind == :presence}
+    f.instance_variable_set(:@mandatory, (validator.attributes if validator) || [])
+  end
 end
