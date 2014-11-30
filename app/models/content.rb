bad12fe86aacefa39601ce19f6ed59eba6aa7a80
Jenua Boiko <jeyboy1985@gmail.com>
2014-11-30 23:34:15 +0300

    fixes

--- app/models/content.rb
+++ app/models/content.rb
@@ -4,4 +4,6 @@ class Content < ActiveRecord::Base
   has_many :content_picts, dependent: :destroy
 
   scope :without_static, -> {where.not(content_type: Content.content_types['static'])}
+
+  validates :name, uniqueness: true, if: -> { self.content_type == 'static' }
 end
