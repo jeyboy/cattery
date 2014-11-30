24f9cf188ba8fa41c2d352680816ea22341c5246
Jenua Boiko <jeyboy1985@gmail.com>
2014-12-01 00:12:49 +0300

    improves and fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  prepend_before_filter -> {
+    I18n.locale = (params[:locale]) if params[:locale]
+  }
+
+  def default_url_options(options={})
+    { locale: I18n.locale }
+  end
 private
   def pagination_pre_page
     10
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
 
   namespace :admin do
@@ -12,28 +13,30 @@ Rails.application.routes.draw do
     resources :contents
   end
 
-  resources :contents, only: [:index, :show]
+  scope '(:locale)', :locale => /en|ru/ do
+    resources :contents, only: [:index, :show]
 
-  resource :static_pages, only: [:show]
+    resource :static_pages, only: [:show]
 
-  resource :gallery, controller: 'gallery', only: [:show]
+    resource :gallery, controller: 'gallery', only: [:show]
 
-  scope 'lalka', as: 'pussies' do
-    resources :cats, only: [:index, :show]
+    scope 'lalka', as: 'pussies' do
+      resources :cats, only: [:index, :show]
+    end
+
+    resources :messages, only: [:index, :create]
+
+    root to: 'contents#index'
+
+    get '/news',        to: 'contents#index', content_types: Content.content_types['news'], as: 'news'
+    get '/posts',       to: 'contents#index', content_types: Content.content_types['post'], as: 'posts'
+    get '/ads',         to: 'contents#index', content_types: Content.content_types['ad'], as: 'ads'
+
+    get '/cats',        to: 'cats#index', is_kitty: false, is_cat: true,   as: 'cats'
+    get '/lady-cats',   to: 'cats#index', is_kitty: false, is_cat: false,   as: 'lady-cats'
+    get '/kittens',     to: 'cats#index', is_kitty: true,   as: 'kittens'
+    get '/guest-book',  to: 'messages#index', as: 'guest_book'
+    get '/links',       to: 'static_pages#show', page: 'links', as: 'links'
+    get '/contacts',    to: 'static_pages#show', page: 'contacts', as: 'contacts'
   end
-
-  resources :messages, only: [:index, :create]
-
-  root to: 'contents#index'
-
-  get '/news',        to: 'contents#index', content_types: Content.content_types['news'], as: 'news'
-  get '/posts',       to: 'contents#index', content_types: Content.content_types['post'], as: 'posts'
-  get '/ads',         to: 'contents#index', content_types: Content.content_types['ad'], as: 'ads'
-
-  get '/cats',        to: 'cats#index', is_kitty: false, is_cat: true,   as: 'cats'
-  get '/lady-cats',   to: 'cats#index', is_kitty: false, is_cat: false,   as: 'lady-cats'
-  get '/kittens',     to: 'cats#index', is_kitty: true,   as: 'kittens'
-  get '/guest-book',  to: 'messages#index', as: 'guest_book'
-  get '/links',       to: 'static_pages#show', page: 'links', as: 'links'
-  get '/contacts',    to: 'static_pages#show', page: 'contacts', as: 'contacts'
 end
