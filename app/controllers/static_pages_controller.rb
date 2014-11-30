73f94b5ce131cff6441f94945721992d04d139a1
Jenua Boiko <jeyboy1985@gmail.com>
2014-11-30 23:21:10 +0300

    fixes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def show
     @page = Content.static.where(name: params[:page]).first
-    redirect_to root_path, alert(t('page.not_found')) unless @page
+    redirect_to root_path, alert: t('page.not_found') unless @page
   end
 end
