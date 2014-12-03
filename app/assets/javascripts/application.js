1f7a90502cdf630dc8b0e4f054a1261590238569
Jenua Boiko <jeyboy1985@gmail.com>
2014-12-03 12:03:24 +0300

    fix background

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,15 @@
 
 //= require ./common
 //= require ./notifies
+
+function resizeBackground() {
+    var $body = $('body');
+    console.log($body);
+    $body.height($body.height() + 60);
+}
+
+$(window).on('resize', resizeBackground);
+
+$(window).ready(function() {
+    resizeBackground();
+});
